ba481adeb7c0f0954115b013a3bdf96ca0672341
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-11-08 17:54:24 -0300

    q2 atividade

--- lista_de_atividades/q2.c
+++ lista_de_atividades/q2.c
@@ -3,29 +3,23 @@
 
 //2) Faça um programa em C que receba 2 números, calcule e mostre a divisão do
 //   primeiro número pelo segundo. Sabe-se que não é permitido o número zero.
-int primeiro_numero, segundo_numero, total ;
+float primeiro_numero, segundo_numero, total ;
   
-int intercao(){
+void interacao(){
  printf("digite o primeiro numero : ");
- scanf("%d",primeiro_numero);
+ scanf("%f",&primeiro_numero);
  printf("digite o segundo numero : ");
- scanf("%d",segundo_numero);
- total= primeiro_numero/segundo_numero;
- return total;
-
+ scanf("%f",&segundo_numero);
+ if (segundo_numero==0){
+    printf("zero no divisor nao e uma divisao valida outra vez ");
+    interacao();
+    }
+ else {
+    total= primeiro_numero/segundo_numero;
+    printf("o resultado e : %f", total);
+    }
 }
 
-
 void main(){
-
- intercao() ;
-
-  
-  
-
-
-
-
-
-
+ interacao() ;
 }
