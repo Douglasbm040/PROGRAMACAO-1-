7384cdfcfdd69b640c689a73be30ecc2d66a4ba0
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-11-25 21:45:42 -0300

    create a.3.c

---
+++ lista_de_atividades_2/a.3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int num_1,num_2;
+  
+    printf("digite o numero : ");
+    scanf("%d",&num_1);
+    printf("digite o numero : ");
+    scanf("%d",&num_2);
+
+    if (num_1 > num_2 ){
+        printf("%d e maior que %d",num_1 ,num_2);
+    }else if(num_1 == num_2){
+        printf("%d e igual a %d", num_1 ,  num_2);
+    }
+    else{
+        printf("%d e maior que %d",num_2,num_1);
+    }
+
+
+
+
+
+
+
+
+    return 0 ;
+}
