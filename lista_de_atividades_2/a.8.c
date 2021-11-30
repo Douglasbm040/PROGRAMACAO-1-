55124d0cc811d5d8e06ed464b2d05dff8e3952a1
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-11-30 11:42:52 -0300

    upgrade a8

--- lista_de_atividades_2/a.8.c
+++ lista_de_atividades_2/a.8.c
@@ -10,19 +10,19 @@ int main(){
     
    if ( saldo > 400  ){    
        valor_do_credito = saldo + (saldo * 0.3  );
-       printf("o valor do credito e : %.2f ",valor_do_credito);
+       printf("1 o seu saldo e %.2f o valor do credito e  %.2f ",saldo,valor_do_credito);
    
    }else if(saldo <= 400 && saldo > 300 ) {       
        valor_do_credito = saldo + (saldo * 0.25 );
-       printf("o valor do credito e : %.2f ",valor_do_credito);
+       printf("2 o seu saldo e %.2f o valor do credito e  %.2f ",saldo,valor_do_credito);
    
    }else if (saldo <= 300 && saldo > 200 ){
        valor_do_credito = saldo + (saldo * 0.2 );
-       printf("o valor do credito e : %.2f ",valor_do_credito);
+       printf("3 o seu saldo e %.2f o valor do credito e  %.2f ",saldo,valor_do_credito);
    
-   }else if ( saldo <= 200 && saldo < 0 ){
+   }else if ( saldo <= 200 && saldo > 0 ){
        valor_do_credito = saldo + (saldo * 0.1 );
-       printf("o valor do credito e : %.2f ",valor_do_credito);
+       printf("4 o seu saldo e %.2f o valor do credito e  %.2f ",saldo,valor_do_credito);
    }
 
     return 0;
