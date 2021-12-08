7641b6b4b75bbd45333064368a5140f3eb6a707d
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2021-12-08 15:17:48 -0300

    a.33.c

---
+++ lista_de_atividades_2/a.33.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    
+    printf("digite um numero : ");
+    scanf("%d",&num);
+
+    if(num > 100){
+        printf("%d",num);
+    }else{
+        printf("0");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
