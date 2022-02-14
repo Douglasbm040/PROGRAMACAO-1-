3f77aa45f44ed75ba0bf3a647444869e959d7591
Douglasbm040 <76476827+Douglasbm040@users.noreply.github.com>
2022-02-14 14:01:13 -0300

    Create a8.c

---
+++ lista_de_atividades_5/a8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define QUAD 6
+//! Faça um programa em C que lê uma matriz M[6,6] e um valor A e multiplica a matriz M pelo valor A e
+//! coloca os valores da matriz multiplicados por A em um vetor V[36] e escreve no final o vetor V.
+
+int main(){
+    int m[QUAD][QUAD],i,j;
+    for(){
+        for(){
+
+        }
+    }
+
+
+
+
+    return 0 ;
+}
