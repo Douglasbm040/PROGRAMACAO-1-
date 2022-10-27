5a0b89e776ec8bb2ef098288ffb67a8a789ae826
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-10-27 08:08:05 -0300

    respostas da questao 7

--- EstruturasDeDados/trabalho_de_C/q7.c
+++ EstruturasDeDados/trabalho_de_C/q7.c
@@ -1,8 +1,31 @@
 #include<stdio.h>
 
 void main (){
+    int matriz[5][5] ;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5 ; j++)
+        {
+            printf("digite um valor inteiro : ");
+            scanf("%d",&matriz[i][j]);
+
+        }
+    }
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5 ; j++)
+        {
+            if (matriz[i][j]% 2 != 0)
+            {
+                printf("valor : %d , indice linha : %d ,indice da coluna : %d ",matriz[i][j] , i , j);
+            }
+            
+        }
+    }
     
 
 
 
-}
+}                                                                       
